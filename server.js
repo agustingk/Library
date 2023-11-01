af5b6abcc4d0deb692105d79910f35d4db4cb48f
Agustin <69488102+agustingk@users.noreply.github.com>
2023-11-01 15:47:08 -0300

    BodyParser

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
 }
 
-
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./Routes/index')
+const authourRouter = require('./Routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/Views')
 app.set('layout', 'Layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('Public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
@@ -22,5 +24,8 @@ db.on('error', error => console.error(error))
 db.once('open', error => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/Authors', authourRouter)
 
 app.listen(process.env.PORT || 3000)
+
+
