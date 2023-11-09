c4d34c8ece0da9fb596798a95b76b16cb872d673
Agustin <69488102+agustingk@users.noreply.github.com>
2023-11-09 15:22:14 -0300

    Update file uploads and ignore unnecessary files

--- Routes/books.js
+++ Routes/books.js
@@ -1,19 +1,18 @@
 const express = require('express')
 const router = express.Router()
-const multer = require('multer')
-const path = require('path')
-const fs = require('fs')
 const Book = require('../Models/book') 
 const Author = require('../Models/author')
 const { listenerCount } = require('process')
-const uploadPath = path.join('public', Book.coverImageBasePath)/// path.join() joins the path of the file with the path of the folder /// public is the folder where the images are stored /// Book.coverImageBasePath is the name of the folder where the images are stored
 const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif'] /// only this types of images are allowed
-const upload = multer({/// multer is a middleware that handles multipart/form-data /// upload is the name of the middleware
-    dest: uploadPath,
-    fileFilter: (req, file, callback) => {//// fileFilter is a function that filters the files that are uploaded
-        callback(null, imageMimeTypes.includes(file.mimetype))/// null is the error /// imageMimeTypes.includes(file.mimetype) checks if the file is an image
-    }
-})
+// const path = require('path')
+// const fs = require('fs')
+// const uploadPath = path.join('public', Book.coverImageBasePath)/// path.join() joins the path of the file with the path of the folder /// public is the folder where the images are stored /// Book.coverImageBasePath is the name of the folder where the images are stored
+// const upload = multer({/// multer is a middleware that handles multipart/form-data /// upload is the name of the middleware
+//     dest: uploadPath,
+//     fileFilter: (req, file, callback) => {//// fileFilter is a function that filters the files that are uploaded
+//         callback(null, imageMimeTypes.includes(file.mimetype))/// null is the error /// imageMimeTypes.includes(file.mimetype) checks if the file is an image
+//     }
+// })
 
 /// All books route
 router.get('/', async (req, res) => {
@@ -46,32 +45,35 @@ router.get('/new', async (req, res) => {
 
 
 //// Create book route
-router.post('/', upload.single('cover'), async (req, res) => { //// upload.single('cover') is the middleware that handles the file upload /// 'cover' is the name of the input in the form
-    const fileName = req.file != null  ? req.file.filename : null
+router.post('/', async (req, res) => { //// upload.single('cover') is the middleware that handles the file upload /// 'cover' is the name of the input in the form
+    // const fileName = req.file != null  ? req.file.filename : null
     const book = new Book({
         title: req.body.title,
         author: req.body.author,
         description: req.body.description,
         publishDate: new Date(req.body.publishDate),
-        coverImageName: fileName,
         pageCount: req.body.pageCount,
-    })  
+    }) 
+
+    saveCover(book, req.body.cover)
+
     try{
         const newBook = await book.save()
         res.redirect('Books')
     } catch {
-        if(book.coverImageName != null){
-            removeBookCover(book.coverImageName)
-        }
+        // if(book.coverImageName != null){
+        //     removeBookCover(book.coverImageName)
+        // }
         renderNewPage(res, book, true)
     }
 })
 
-function removeBookCover(fileName){
-    fs.unlink(path.join(uploadPath, fileName), err =>{
-        if(err) console.error(err)
-    })
-}
+
+// function removeBookCover(fileName){
+//     fs.unlink(path.join(uploadPath, fileName), err =>{
+//         if(err) console.error(err)
+//     })
+// }
 
 async function renderNewPage(res, book, hasError = false) {
     try {
@@ -84,6 +86,16 @@ async function renderNewPage(res, book, hasError = false) {
         res.render('Books/new', params)
     } catch {
         res.redirect('/Books')
+    }
+}
+
+function saveCover(book, coverEncoded){
+    if(coverEncoded ==  null) return
+
+    const cover = JSON.parse(coverEncoded)
+    if(coverEncoded != null && imageMimeTypes.includes(cover.type)){
+        book.coverImage = new Buffer.from(cover.data, 'base64')
+        book.coverImageType = cover.type
     }
 }
 
